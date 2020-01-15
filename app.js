1c17d8771e26ffa165a6ca2550940bd55767f858
Francisco Chiarino <francisco.chiarino@gmail.com>
2020-01-15 13:39:57 +0100

    Setup variables

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
-let display = document.getElementById('display');
-const array = ['_', '_', '_', '_'];
-display.innerHTML += `<h1> ${array.join(' ')}</h1>`;
+// Variables
+
+const display = document.getElementById('display');
+const head = document.getElementById('head');
+const body = document.getElementById('body');
+const leftArm = document.getElementById('arm-left');
+const rightArm = document.getElementById('arm-right');
+const leftLeg = document.getElementById('leg-left');
+const leftArm = document.getElementById('leg-right');
+
+
+
+// How to write in HTML
+// let display = document.getElementById('display');
+// const array = ['_', '_', '_', '_'];
+// display.innerHTML += `<h1> ${array.join(' ')}</h1>`;
