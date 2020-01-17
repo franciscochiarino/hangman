2c9d07401ec1392f6e7644621bc2c847cdcfbc00
fran <francisco.chiarino@gmail.com>
2020-01-17 14:53:28 +0100

    Create possibleWord

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const rightLeg = document.getElementById('leg-right');
 
 
 // Local variables
+const possibleWord = [['J', 'A', 'V', 'A', 'S', 'C', 'R', 'I', 'P', 'T'], ['F', 'R', 'A', 'N', 'C', 'I', 'S', 'C', 'O']]; // Any word we want to be guessed.
 const letters = ['J', 'A', 'V', 'A', 'S', 'C', 'R', 'I', 'P', 'T']; // Any word we want to be guessed.
 const guesses = ['_', '_', '_', '_', '_', '_', '_', '_', '_', '_']; // Blank letter spaces to be completed if letter is guessed.
 let counter = 0; // Checks how many times the player fails.
